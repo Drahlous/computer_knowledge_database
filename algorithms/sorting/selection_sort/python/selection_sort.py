2ee82e5f838f55ad9afdbde163ed3a928ded4121
Drahlous <MattTStachowiak@gmail.com>
2021-12-26 18:53:08 -0700

    Add selection_sort unit testing

--- algorithms/sorting/selection_sort/python/selection_sort.py
+++ algorithms/sorting/selection_sort/python/selection_sort.py
@@ -1,3 +1,5 @@
+import unittest
+
 
 def find_smallest_idx(input_array):
     smallest = input_array[0]
@@ -19,5 +21,24 @@ def selection_sort(input_array):
         new_array.append(input_array.pop(smallest_idx))
     return new_array
 
-print(selection_sort([5, 3, 6, 2, 10]))
+
+
+# Unit Tests
+class SelectionSortTest(unittest.TestCase):
+
+    def test_selection_sort_empty(self):
+        self.assertEqual(selection_sort([]), [])
+
+    def test_selection_sort_single(self):
+        self.assertEqual(selection_sort([5]), [5])
+
+    def test_selection_sort_basic(self):
+        self.assertEqual(selection_sort([5, 3, 6, 2, 10]), [2, 3, 5, 6, 10])
+
+    def test_selection_sort_with_negatives(self):
+        self.assertEqual(selection_sort([1, 0, -2, 2, -1]), [-2, -1, 0, 1, 2])
+
+
+
+
 
