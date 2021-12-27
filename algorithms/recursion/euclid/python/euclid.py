481a038e485116d150b060b566b58f37f1fbb8dd
Drahlous <MattTStachowiak@gmail.com>
2021-12-26 19:29:02 -0700

    Add unit testing for euclid

--- algorithms/recursion/euclid/python/euclid.py
+++ algorithms/recursion/euclid/python/euclid.py
@@ -1,15 +1,16 @@
 #!/bin/python
+import unittest
 
 # Divide a rectangle into a set of squares with the largest size possible.
 # The squares must fill the entire area and be a uniform size.
 def euclid(width: int, height: int):
     ### Base Cases
-    if width == 0:
-        return (height, height)
+    if width == height:
+        return width
+    elif width == 0:
+        return height
     elif height == 0:
-        return (width, width)
-    elif width == height:
-        return (width, height)
+        return width
 
 
     ### Recursive Cases
@@ -23,17 +24,31 @@ def euclid(width: int, height: int):
         height -= width*(int(height/width))
         return euclid(width, height)
 
-# Expect (50, 50)
-print(euclid(50, 50))
-print(euclid(100, 50))
-print(euclid(150, 50))
-print(euclid(50, 150))
-print(euclid(250, 150))
 
-# Expect (10, 10)
-print(euclid(10, 100))
-print(euclid(100, 10))
+# Unit Tests
+class EuclidTest(unittest.TestCase):
+    def test_square_50(self):
+        self.assertEqual(euclid(50, 50), 50)
 
-# Expect (1, 1)
-print(euclid(3, 5))
+    def test_rect_100_50(self):
+        self.assertEqual(euclid(100, 50), 50)
+
+    def test_rect_150_50(self):
+        self.assertEqual(euclid(150, 50), 50)
+
+    def test_rect_50_150(self):
+        self.assertEqual(euclid(50, 150), 50)
+
+    def test_rect_250_150(self):
+        self.assertEqual(euclid(250, 150), 50)
+
+    def test_rect_10_100(self):
+        self.assertEqual(euclid(10, 100), 10)
+
+    def test_rect_100_10(self):
+        self.assertEqual(euclid(100, 10), 10)
+
+    # Expect (1, 1)
+    def test_rect_3_5(self):
+        self.assertEqual(euclid(3, 5), 1)
 
