9cc92ab7b0d5adde0d10de098d6b211a490f2b87
Drahlous <MattTStachowiak@gmail.com>
2021-12-26 19:14:16 -0700

    Add unit testing for binary search

--- algorithms/binary_search/rust/src/main.rs
+++ algorithms/binary_search/rust/src/main.rs
@@ -1,65 +1,33 @@
-use std::io::{self};
-use serde::{Deserialize, Serialize};
-//use serde_json::{Result, Value};
-
-
-#[derive(Serialize, Deserialize)]
-struct InputArray {
-    entries: Vec<u8>,
-    target: u8
-}
-
-fn binary_search(list: &Vec<u8>, start: usize, end: usize, target: u8) {
+fn binary_search(list: &Vec<u8>, start: usize, end: usize, target: u8) -> Option<usize> {
     let index = (start + end) / 2;
 
-    println!("start: {}, end: {}", start, end);
-    println!("index: {}", index);
-
-    if list[index] == target {
-       println!("found target {} at index {}", target, index);
-       return;
-    }
-    else if start == end {
-       println!("could not find target {}", target);
-       return;
-    }
-    else if list[index] < target {
-        binary_search(list, index + 1, end, target);
-    }
-    else {
-        binary_search(list, start, index - 1, target);
+    match list[index] {
+        item => {
+            if item == target {
+                Some(index)
+            } else if start == end {
+                None
+            } else if item < target {
+                binary_search(list, index + 1, end, target)
+            } else {
+                binary_search(list, start, index - 1, target)
+            }
+        }
     }
 }
 
-
-fn main() -> io::Result<()> {
-
-    // Read input from stdin
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
-
-    
-    // Extract "entries" array from input json
-    let input: InputArray = serde_json::from_str(&buffer)?;
-
-    println!("\n\nTest Input");
-    let entries = input.entries;
-    println!("entries: {:?}", entries);
-
-    let target = input.target;
-    println!("target: {}", target);
-
-    let start_index = 0;
-    let end_index = entries.len() - 1;
-
-    println!("Test Start");
-    binary_search(&entries, 
-                  start_index, 
-                  end_index,
-                  target);
-
-    println!("Test End");
-    
-    Ok(())
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_binary_search_basic() {
+        let entries: Vec<u8> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].to_vec();
+        let start_index = 0;
+        let end_index = entries.len() - 1;
+        let target = 3;
+        assert_eq!(
+            binary_search(&entries, start_index, end_index, target),
+            Some(3)
+        )
+    }
 }
-
